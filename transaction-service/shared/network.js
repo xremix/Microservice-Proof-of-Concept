6a33077bfd2c1334864e4f1619f2d1350eac5c99
xremix <tonidhoffmann@gmail.com>
2018-04-23 09:23:58 +0200

    shared network

---
+++ transaction-service/shared/network.js
@@ -0,0 +1,20 @@
+var request = require('request');
+var env = require('./shared/env');
+
+var token = "";
+
+var get = function(port, url, callback, errorFunction) {
+  !callback && console.log("No Callback function defined for " + url);
+  !errorFunction && console.log("No Error function defined for " + url);
+  request('http://'+env.get("HOSTIP")+':'+port+url+"?token="+token, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      callback && callback(JSON.parse(body));
+    }else{
+      console.log(error);
+      errorFunction && errorFunction(error);
+    }
+  })
+};
+
+module.exports.get = get;
+module.exports.token = token;
