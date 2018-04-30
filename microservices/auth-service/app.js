397d767b17b515a660b01c2f62a723fe420f3d7e
xremix <tonidhoffmann@gmail.com>
2018-04-30 16:22:30 +0200

    started working with correlator

--- microservices/auth-service/app.js
+++ microservices/auth-service/app.js
@@ -9,21 +9,22 @@ const HOST = '0.0.0.0';
 
 // App
 const app = express();
+app.use(correlator());
 
 app.get('/', (req, res) => {
     res.send("Hello Auth");
 });
 
 app.get('/auth', (req, res) => {
-  logger.log("Client authenticated. Generating GUID", -1);
+  logger.log("Client authenticated. Generating GUID", req.correlationId());
   res.send("df6bd192-a5f5-4250-817c-24a682d9143a");
 });
 app.get('/checkauth/', (req, res) => {
   if(req.query.token == "df6bd192-a5f5-4250-817c-24a682d9143a"){
-    logger.log("token " + req.query.token + " valid", -1);
+    logger.log("token " + req.query.token + " valid", req.correlationId());
     res.send({status: "success"});
   }else{
-    logger.log("token " + req.query.token + " not valid", -1);
+    logger.log("token " + req.query.token + " not valid", req.correlationId());
     res.status(401).send({status: "wrong-token"});
   }
 });
